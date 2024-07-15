3c207e456bd68d610d99a58aab5f64375f5ca38e
Shreyansh <srivastavashreyansh30@gmail.com>
2024-07-15 04:15:04 -0500

    initial

--- src/index.js
+++ src/index.js
@@ -37,6 +37,9 @@ function displayTodos() {
         const todo_div = document.createElement('div');
         todo_div.classList.add('todo-div');
         
+        const todo_ele = document.createElement('div');
+        todo_ele.classList.add('todo-ele');
+        
         const todo_title = document.createElement('h3');
         todo_title.innerText = todo.title;
         const todo_description = document.createElement('p');
@@ -44,11 +47,33 @@ function displayTodos() {
         const todo_priority = document.createElement('p');
         todo_priority.innerText = `Priority: ${todo.priority}`;
 
+        const delete_button = document.createElement('button');
+        delete_button.classList.add('delete-button');
+        delete_button.innerText = "Delete task"
+
+        todo_div.appendChild(todo_ele);
+        todo_ele.appendChild(todo_title);
+        todo_ele.appendChild(todo_description);
+        todo_ele.appendChild(todo_priority);
+        todo_div.appendChild(delete_button);
+        
         todos_main_container.appendChild(todo_div);
-        todo_div.appendChild(todo_title);
-        todo_div.appendChild(todo_description);
-        todo_div.appendChild(todo_priority);
+
+        // const delete_btn = document.querySelector('.delete-button');
+        delete_button.addEventListener('click', () => {
+            deleteTodo(todo);
+            displayTodos();
+        })
         
     });
-
 }
+
+function deleteTodo(todo){
+    console.log("delete")
+    const todoIndex = todos.findIndex(t => t.title === todo.title);
+    if (todoIndex != -1) {
+        todos.splice(todoIndex, 1);
+    }
+    else console.log("Todo not found!");
+}
+
